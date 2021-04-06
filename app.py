6752948db61ad69f8c46523ab7333367dcda77e9
sunny-aguilar <sunny.aguilar@gmail.com>
2021-04-05 22:05:51 -0700

    cs361-project

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ from flask import Flask, render_template
 app = Flask(__name__, static_url_path='/static')
 
 
+
+
+
+
+
 #-------------------------------------------------------------------------
 # Start Web App
 if __name__ == "__main__":
