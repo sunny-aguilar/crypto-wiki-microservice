20871e4715df81bf40bf43b60fe2fd677be54766
sunny-aguilar <sunny.aguilar@gmail.com>
2021-04-05 22:05:57 -0700

    cs361-project

--- app.py
+++ app.py
@@ -17,6 +17,18 @@ app = Flask(__name__, static_url_path='/static')
 
 
 
+#-------------------------------------------------------------------------
+# routes in blueprints
+# route errors -----
+@app.errorhandler(404)
+def page_not_found(error):
+  msg = "Oh snap, the page you requested was not found"
+  return render_template('not_found.html', results=msg), 404
+
+@app.errorhandler(500)
+def server_error(error):
+  msg = "Oh snap, it looks like the programmers made a boo boo. Go to the main page to clear this up."
+  return render_template('server_error.html', results=msg), 500
 
 
 
