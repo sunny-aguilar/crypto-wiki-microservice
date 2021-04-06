c8aa9ff89cebfd804a5d6b4e304a49c8d13da9c2
sunny-aguilar <sunny.aguilar@gmail.com>
2021-04-05 22:03:13 -0700

    cs361-project

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # Authors:                                Sandro Aguilar
 # Date:                                   April 5, 2021
 # Class:                                  CS 361
-# Description:                            Group project that implements a relational database using
-#                                         MySQL. Python is used for the back-end with Flask as the
+# Description:                            Pproject that implements a microservices strategy with
+#                                         my other team members.
 #                                         framework.
 
 
