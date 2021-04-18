2e80f209ffcb01bdd656a7867dca3370df83ece5
sunny-aguilar <sunny.aguilar@gmail.com>
2021-04-17 21:44:10 -0700

    cs361-project

--- mod_index/index.py
+++ mod_index/index.py
@@ -6,8 +6,8 @@
 #
 
 #-------------------------------------------------------------------------
-
-
+# import the required libraries for the web app and other modules
+from flask import redirect, url_for, request, render_template, Blueprint
 
 
 
