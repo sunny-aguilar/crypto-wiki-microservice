cd6507ef7cd86a650fa3eb17e6485e3e6c01bf07
sunny-aguilar <sunny.aguilar@gmail.com>
2021-04-17 21:52:51 -0700

    cs361-project

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from flask import Flask, render_template
 app = Flask(__name__, static_url_path='/static')
 
 
+#-------------------------------------------------------------------------
 # register blueprint modules
 
 
