527b1680e407f71a8a95da716ef49d26f8a8dabd
sunny-aguilar <sunny.aguilar@gmail.com>
2021-04-18 16:19:20 -0700

    cs361-project

---
+++ mod_gui/gui.py
@@ -0,0 +1,9 @@
+# Authors:                                Sandro Aguilar
+# Date:                                   April 18, 2021
+# Class:                                  CS 361
+# Description:                            This module holds the GUI page for the web app
+#                                         and is a separate blueprint module.
+#
+
+#-------------------------------------------------------------------------
+# import the required libraries for the web app and other modules
