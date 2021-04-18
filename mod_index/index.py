5cd35505f0c4b2b0f1c2560797a39fbc989ed1ba
sunny-aguilar <sunny.aguilar@gmail.com>
2021-04-17 21:39:53 -0700

    cs361-project

--- mod_index/index.py
+++ mod_index/index.py
@@ -1,5 +1,5 @@
 # Authors:                                Sandro Aguilar
-# Date:                                   April 18, 2021
+# Date:                                   April 17, 2021
 # Class:                                  CS 340
 # Description:                            Group project that implements a relational database using
 #                                         SQL. Python is used for the back-end.
