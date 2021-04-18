74be2e2f16cf8ec362425f7e30ef0be286f17eee
sunny-aguilar <sunny.aguilar@gmail.com>
2021-04-17 21:40:46 -0700

    cs361-project

--- mod_index/index.py
+++ mod_index/index.py
@@ -2,7 +2,7 @@
 # Date:                                   April 17, 2021
 # Class:                                  CS 361
 # Description:                            This module holds the index page for the web app
-#                                         SQL. Python is used for the back-end.
+#                                         and is a separate blueprint module.
 #
 
 #-------------------------------------------------------------------------
