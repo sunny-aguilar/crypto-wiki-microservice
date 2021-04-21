bae552b92da3236d5e9aa53b073293c5825d3032
sunny-aguilar <sunny.aguilar@gmail.com>
2021-04-20 23:04:55 -0700

    cs361-project

--- mod_api/api.py
+++ mod_api/api.py
@@ -23,11 +23,23 @@ def api():
   print('API endpoint accessed!')
 
   msg = 'GUI page.'
-  books = {
-    1: "Sandro",
-    2: "Aguilar",
-    'motto': 'Do or do not do.'
-  }
+  books = [
+    {'id': 0,
+      'title': 'A Fire Upon the Deep',
+      'author': 'Vernor Vinge',
+      'first_sentence': 'The coldsleep itself was dreamless.',
+      'year_published': '1992'},
+    {'id': 1,
+      'title': 'The Ones Who Walk Away From Omelas',
+      'author': 'Ursula K. Le Guin',
+      'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+      'published': '1973'},
+    {'id': 2,
+      'title': 'Dhalgren',
+      'author': 'Samuel R. Delany',
+      'first_sentence': 'to wound the autumnal city.',
+      'published': '1975'}
+]
   # return redirect('/')
   # return render_template('/', msg=msg)
   return jsonify(books)
