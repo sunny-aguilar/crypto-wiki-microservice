d4a56015c32279dd53572bd6689ec82bdea935ef
sunny-aguilar <sunny.aguilar@gmail.com>
2021-04-20 21:26:54 -0700

    cs361-project

--- mod_api/api.py
+++ mod_api/api.py
@@ -1,9 +1,12 @@
 # Authors:                                Sandro Aguilar
 # Date:                                   April 20, 2021
 # Class:                                  CS 361
-# Description:                            This module holds the index page for the web app
+# Description:                            This module holds the api endpoint for the web app
 #                                         and is a separate blueprint module.
 #
 
 #-------------------------------------------------------------------------
 # import the required libraries for the web app and other modules
+from flask import redirect, url_for, request, render_template, Blueprint
+
+
