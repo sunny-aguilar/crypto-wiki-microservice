beeff3c826e49adad7b6f9aac4efba7447edd458
sunny-aguilar <sunny.aguilar@gmail.com>
2021-04-22 11:56:57 -0700

    cs361-project

--- mod_docs/docs.py
+++ mod_docs/docs.py
@@ -13,7 +13,7 @@ from flask import redirect, url_for, request, render_template, Blueprint
 gui_docs = Blueprint('gui_docs', __name__, template_folder='templates')
 
 # blueprint routes
-@gui_docs.route('/docs', methods=['GET'])
+@mod_docs.route('/docs', methods=['GET'])
 def gui():
   docs = 'active'
   return render_template('gui.html', docs_menu=docs)
