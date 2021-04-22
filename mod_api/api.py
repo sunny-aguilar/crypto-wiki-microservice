689db4b7d782607b3690ad29bb6237f0be735c7b
sunny-aguilar <sunny.aguilar@gmail.com>
2021-04-21 21:20:54 -0700

    cs361-project

--- mod_api/api.py
+++ mod_api/api.py
@@ -42,7 +42,11 @@ def api():
       print(page.summary)
       print(page.text)
 
-
+    def print_sections(sections, level=0):
+      for s in sections:
+        print("%s: %s - %s" % ("*" * (level + 1), s.title, s.text[0:40]))
+        print_sections(s.sections, level + 1)
+    print_sections(page.sections)
 
     books = [
       {'id': 0,
