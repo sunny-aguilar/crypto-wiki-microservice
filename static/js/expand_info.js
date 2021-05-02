59a77727b2cb6167e582abb6af077b9f17e77ed4
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-01 22:22:08 -0700

    cs361-project

--- static/js/expand_info.js
+++ static/js/expand_info.js
@@ -1,1 +1,10 @@
 // author:   Sandro Aguilar
+
+// event listener to wait for DOM to load before running script
+document.addEventListener("DOMContentLoaded", runScript, false);
+
+// function that adds an event listener for click events
+function runScript(event) {
+  const parent = document.body;
+  parent.addEventListener("click", addRow, false);
+}
