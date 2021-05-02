58a2248cfaf337d0f43714ecb1d4e95d91af1d76
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-01 22:40:35 -0700

    cs361-project

--- static/js/expand_info.js
+++ static/js/expand_info.js
@@ -13,6 +13,9 @@ function expandRow(event) {
   const ele = event.target;
   console.log(event.target);
   if (ele != '<i class="lni lni-arrow-down-circle"></i>') {
+    console.log('Wrong target!');
     return;
   }
+
+  console.log("Target found!");
 }
