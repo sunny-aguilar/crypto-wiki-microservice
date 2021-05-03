2114e7fcd81658bfb6ad72e9cdaf4cdeaebc13e5
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-03 14:55:44 -0700

    cs361-project

---
+++ mod_api/api_helper.py
@@ -0,0 +1,10 @@
+# Authors:                                Sandro Aguilar
+# Date:                                   April 20, 2021
+# Class:                                  CS 361
+# Description:                            This file contains helper functions to
+#                                         implement the Wikiscraper.
+
+#-------------------------------------------------------------------------
+# import the required libraries for the web app and other modules
+from bs4 import BeautifulSoup
+import requests
