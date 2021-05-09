1106b06cf18352693c9b80d08672ee9e45e377ad
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-08 21:28:38 -0700

    cs361-project

--- static/js/cryptoData.js
+++ static/js/cryptoData.js
@@ -11,7 +11,7 @@ const cryptoData = {
   bitcoin:{name: "Bitcoin", image: "/static/img/btc.png", supply: 18696643},
   ethereum: {name: "Ethereum", image: "/static/img/eth.png", supply: 115798024},
   litecoin: {name: "Litecoin", image: "/static/img/ltc.png", supply: 66752415},
-  dai: {name: "Dai", image: "/static/img/dai.png", supply: 0},
+  dai: {name: "Dai", image: "/static/img/dai.png", supply: 4593097212},
   usdt: {name: "USDT", image: "/static/img/usdt.png", supply: 0},
   chainlink: {name: "Chainlink", image: "/static/img/link.png", supply: 419009556},
   uniswap: {name: "Uniswap", image: "/static/img/uni.png", supply: 523384244},
