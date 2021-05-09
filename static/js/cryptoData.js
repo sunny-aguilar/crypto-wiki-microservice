05dc95b4c76efc6a1905ec930e3a999250067168
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-08 21:23:25 -0700

    cs361-project

--- static/js/cryptoData.js
+++ static/js/cryptoData.js
@@ -9,13 +9,13 @@
 const cryptoNames = ["Bitcoin", "Ethereum", "Litecoin", "Dai", "USDT", "Chainlink", "Uniswap", "PancakeSwap", "Pancake Bunny", "BNB"];
 const cryptoData = {
   bitcoin:{name: "Bitcoin", image: "/static/img/btc.png", supply: 18696643},
-  ethereum: {name: "", image: "/static/img/.png", supply: 0},
-  litecoin: {name: "", image: "/static/img/.png", supply: 0},
+  ethereum: {name: "", image: "/static/img/eth.png", supply: 0},
+  litecoin: {name: "", image: "/static/img/ltc.png", supply: 0},
   dai: {name: "", image: "/static/img/.png", supply: 0},
   usdt: {name: "", image: "/static/img/.png", supply: 0},
   chainlink: {name: "", image: "/static/img/.png", supply: 0},
   uniswap: {name: "", image: "/static/img/.png", supply: 0},
   pancakeswap: {name: "", image: "/static/img/.png", supply: 0},
   pancakebunny: {name: "", image: "/static/img/.png", supply: 0},
-  bnb: {name: "", image: "/static/img/.png", supply: 0}
+  bnb: {name: "", image: "/static/img/bnb.png", supply: 0}
 }
