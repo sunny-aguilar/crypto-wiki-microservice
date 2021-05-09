38b0220fea4a37d53c09cfb81d5a85a2a735651f
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-08 21:11:55 -0700

    cs361-project

--- static/js/cryptoData.js
+++ static/js/cryptoData.js
@@ -8,3 +8,8 @@
 // event listener to wait for DOM to load before running script
 document.addEventListener("DOMContentLoaded", runScript, false);
 
+// function that adds an event listener for click events
+function runScript(event) {
+  const parent = document.body;
+  parent.addEventListener("click", expandRow, false);
+}
