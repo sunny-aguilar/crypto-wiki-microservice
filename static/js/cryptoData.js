0f3e4a2f584df7bf196040dac0ba7672b9a0f06f
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-08 21:10:45 -0700

    cs361-project

---
+++ static/js/cryptoData.js
@@ -0,0 +1,9 @@
+//  Authors:                                Sandro Aguilar
+//  Date:                                   May 8, 2021
+//  Class:                                  CS 361
+//  Description:                            This JS file is used to hold data on
+//                                          crypto assets that will be listed.
+
+// -------------------------------------------------------------------------
+
+
