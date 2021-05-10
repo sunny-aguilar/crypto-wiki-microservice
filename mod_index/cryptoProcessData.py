8412a12f381878f50666bc29bbf9c08035adfe4b
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-09 22:28:26 -0700

    cs361-project

---
+++ mod_index/cryptoProcessData.py
@@ -0,0 +1,13 @@
+# Authors:                                Sandro Aguilar
+# Date:                                   May 9, 2021
+# Class:                                  CS 361
+# Description:                            This module processes the data obtained
+#                                         from the CMC api to obtain the following
+#                                         data:
+#                                         - coin names
+#                                         - symbols
+#                                         - total supply
+#
+
+#-------------------------------------------------------------------------
+# import the required libraries for the web app and other modules
