93fc54f1b9dcdd5cca74ad432616a62866526b2b
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-12 21:50:10 -0700

    cs361-project

--- static/js/useWikiService.js
+++ static/js/useWikiService.js
@@ -21,5 +21,8 @@ function runScript(event) {
 function useWikiService(event) {
   // url for AJAX request
   const base_url = "http://flip3.engr.oregonstate.edu:55055/api";
+
+  // AJAX request
+  const xhr = new XMLHttpRequest();
 }
 
