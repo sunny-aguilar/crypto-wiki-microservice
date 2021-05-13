a8122d40b2a95eb65c18148e6024f7a2045da124
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-12 21:46:58 -0700

    cs361-project

---
+++ static/js/useWikiService.js
@@ -0,0 +1,12 @@
+//  Authors:                                Sandro Aguilar
+//  Date:                                   May 8, 2021
+//  Class:                                  CS 361
+//  Description:                            This JS file is used send GET requests to the Wiki
+//                                          API service that I have implemented. The code is not
+//                                          called directly but rather an HTTP GET request is made
+//                                          over the internet to request the service via this
+//                                          script.
+// -------------------------------------------------------------------------
+
+
+
