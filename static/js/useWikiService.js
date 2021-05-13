ccaf6fd68ddfd430730333b0cc17346f0d0e528a
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-12 22:58:13 -0700

    cs361-project

--- static/js/useWikiService.js
+++ static/js/useWikiService.js
@@ -21,7 +21,8 @@ function runScript(event) {
 // function that calls Wiki Scraper service
 function useWikiService(event) {
   // url for AJAX request
-  const base_url = "http://flip3.engr.oregonstate.edu:55055/api?";
+  // const base_url = "http://flip3.engr.oregonstate.edu:55055/api?";
+  const base_url = "http://localhost:55055/api?";
 
   // URL parameter settings
   const btc = "search_term=bitcoin&";
