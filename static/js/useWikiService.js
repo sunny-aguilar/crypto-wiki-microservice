e8cb0e363b357a274f5d548fe60d3d5c72ca32fe
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-15 12:24:20 -0700

    cs361-project

--- static/js/useWikiService.js
+++ static/js/useWikiService.js
@@ -23,13 +23,13 @@ function useWikiService(event) {
   // url for AJAX request
   // const base_url = "http://flip3.engr.oregonstate.edu:55055/api?";  // use this URL when saving to OSU servers
   // const base_url = "https://crypto-wiki.herokuapp.com/api?";   // for Heroku
-  const base_url = "http://localhost:55055/api?";   // update to heroku
+  // const base_url = "http://localhost:55055/api?";   // update to heroku
 
   // URL parameter settings
-  const btc = "search_term=bitcoin&";
-  const summary = "summary=on";
-  const url = base_url + btc + summary;
-  console.log(url);
+  // const btc = "search_term=bitcoin&";
+  // const summary = "summary=on";
+  // const url = base_url + btc + summary;
+  // console.log(url);
 
   buildURL("bitcoin")
 
@@ -77,9 +77,10 @@ function buildURL(asset) {
   const base_url = "http://localhost:55055/api?";   // update to heroku
 
   // URL parameter settings
-  const btc = "search_term=bitcoin&";
+  const btc = `search_term=bitcoin&${asset}`;
   const summary = "summary=on";
   const url = base_url + btc + summary;
   console.log(url);
+  return url;
 }
 
