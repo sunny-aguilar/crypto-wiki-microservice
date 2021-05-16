14d244ecbf9ea5f6a23c3e06f270484d88c740a3
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-15 22:43:24 -0700

    cs361-project

--- static/js/useWikiService.js
+++ static/js/useWikiService.js
@@ -91,5 +91,5 @@ function makeRequest(url) {
 //              based on data returned from wiki scraper service.
 // -------------------------------------------------------------------------
 function updateSummary(text, coin_id) {
-  document.getElementById(coin_id).innerText = summary["summary"];
+  document.getElementById(coin_id).innerText = text;
 }
