4c7ab45597368e4be85323511758205ddb8e0c22
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-17 00:35:16 -0700

    cs361-project

--- static/js/cryptoData.js
+++ static/js/cryptoData.js
@@ -7,19 +7,6 @@
 // -------------------------------------------------------------------------
 
 const cryptoNames = ["Bitcoin", "Ethereum", "Litecoin", "Dai", "USDT", "Chainlink", "Uniswap", "PancakeSwap", "Pancake Bunny", "BNB"];
-const cryptoData = {
-  bitcoin:{name: "Bitcoin", image: "/static/img/btc.png", supply: 18696643},
-  ethereum: {name: "Ethereum", image: "/static/img/eth.png", supply: 115798024},
-  litecoin: {name: "Litecoin", image: "/static/img/ltc.png", supply: 66752415},
-  dai: {name: "Dai", image: "/static/img/dai.png", supply: 4593097212},
-  usdt: {name: "USDT", image: "/static/img/usdt.png", supply: 55536494298},
-  polygon: {name: "Polygon", image: "/static/img/usdt.png", supply: 5188388130},
-  chainlink: {name: "Chainlink", image: "/static/img/link.png", supply: 419009556},
-  uniswap: {name: "Uniswap", image: "/static/img/uni.png", supply: 523384244},
-  pancakeswap: {name: "Pancake Swap", image: "/static/img/cake.png", supply: 164396771},
-  pancakebunny: {name: "Pancake Bunny", image: "/static/img/bunny.png", supply: 0},
-  bnb: {name: "BNB", image: "/static/img/bnb.png", supply: 115798024}
-}
 
 const cryptoSummaries = {
   "Polygon": "With polygon, any project can easily spin-up a dedicated blockchain network which combines the best features of stand-alone blockchains (sovereignty, scalability and flexibility) and Ethereum (security, interoperability and developer experience). Additionally, these blockchains are compatible with all the existing Ethereum tools (Metamask, MyCrypto, Remix etc), and can exchange messages among themselves and with Ethereum.Decentralized Apps are making huge progress but the current blockchain ecosystem is not prepared to scale as per the demand. The issues of slow block confirmations and high gas fees need to be solved before we target mass adoption by mainstream users. And most importantly, it needs awesome user experience. We aim to change that by simplifying the interaction between users and the decentralized world. We want to make interacting with the decentralized ecosystem so easy that anyone can do so without worrying about the complexity of the system.",
