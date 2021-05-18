9a7e2c692113fbbdc88bc4004da9a2b0af820c0a
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-17 20:19:53 -0700

    cs361-project

--- mod_index/cryptoProcessData.py
+++ mod_index/cryptoProcessData.py
@@ -16,7 +16,14 @@
 def clean_data(raw_data):
   data = raw_data
 
-  print(data['data'][0]['max_supply'])
+  # get marketcap
+  print(data['data'][0]['circulating_supply'])
+
+  # get price
+  print(data['data'][0]['circulating_supply'])
+
+  # get circulating supply
+  print(data['data'][0]['circulating_supply'])
 
 
 
