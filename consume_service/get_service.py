b909dbd3a4f364e33b64d3979ad654ec73cd40ee
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-21 10:11:59 -0700

    cs361-project

--- consume_service/get_service.py
+++ consume_service/get_service.py
@@ -9,10 +9,15 @@
 #-------------------------------------------------------------------------
 # import the required libraries for the web app and other modules
 from requests import Request
+import requests
 
 def get_prices():
   base_url = 'https://triviabot-converter.herokuapp.com/convert?'
   param_1 = 'crypto1=BTC&curr1=USD&crypto2=ETH&curr2=USD&crypto3=ltc&curr3=USD&crypto4=dai&curr4=USD&crypto5=usdt&curr5=USD'
   param_2 = 'crypto1=BTC&curr1=USD&crypto2=ETH&curr2=USD&crypto3=ltc&curr3=USD&crypto4=dai&curr4=USD&crypto5=usdt&curr5=USD'
-  
+
+  # make GET request to microservice
+  r = requests.get(base_url+param_1)
+  print(r)
+
   return 0
