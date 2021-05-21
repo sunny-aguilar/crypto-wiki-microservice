8c89d6b483d946fa1e81f4b2b830a5817556e5e9
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-21 10:02:40 -0700

    cs361-project

---
+++ consume_service/get_service.py
@@ -0,0 +1,16 @@
+# Authors:                                Sandro Aguilar
+# Date:                                   May 9, 2021
+# Class:                                  CS 361
+# Description:                            This module processes the data obtained
+#                                         from the CMC api to obtain the following
+#                                         data:
+#                                         - coin names
+#                                         - symbols
+#                                         - total supply
+#
+
+#-------------------------------------------------------------------------
+# import the required libraries for the web app and other modules
+
+
+#https://triviabot-converter.herokuapp.com/convert?crypto1=BTC&curr1=USD&crypto2=ETH&curr2=USD&crypto3=ltc&curr3=USD&crypto4=dai&curr4=USD&crypto5=usdt&curr5=USD
