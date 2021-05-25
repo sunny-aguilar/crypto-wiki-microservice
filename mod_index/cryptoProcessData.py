d372bc5c15825da8ae6dfd206e54b4251812c80e
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-24 21:43:38 -0700

    cs361-project

--- mod_index/cryptoProcessData.py
+++ mod_index/cryptoProcessData.py
@@ -4,7 +4,7 @@
 # Description:                            This module processes the data obtained
 #                                         from the CMC api to obtain the following
 #                                         data:
-#                                         - coin price (via mic)
+#                                         - coin price (via micr)
 #                                         - coin names
 #                                         - symbols
 #                                         - total supply
