94a2d5f985fe66d52b01588872fb2da89ca915b8
sunny-aguilar <sunny.aguilar@gmail.com>
2021-05-24 21:46:05 -0700

    cs361-project

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # Class:                                  CS 361
 # Description:                            Pproject that implements a microservices strategy with
 #                                         my other team members. The main project consists of a
-#
+#                                         website that 
 #
 # The software you write will need
 #                                         to communicate (request data, receive data, receive
