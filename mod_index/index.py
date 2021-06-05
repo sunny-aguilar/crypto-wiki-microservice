18c5b103ba938338631af4bdd9e654d2fda3c949
sunny-aguilar <sunny.aguilar@gmail.com>
2021-06-05 10:31:48 -0700

    cs361-project

--- mod_index/index.py
+++ mod_index/index.py
@@ -10,6 +10,7 @@
 from flask import redirect, url_for, request, render_template, Blueprint
 from mod_index.cryptoData import get_crypto_data
 from mod_index.cryptoProcessData import clean_data
+import socket
 
 # create a blueprint module
 index_bp = Blueprint('index_bp', __name__, template_folder='templates')
@@ -20,6 +21,11 @@ def index():
   home = 'active'
   json_crypto_data = get_crypto_data()
   data = clean_data(json_crypto_data)
-  # print(data)
 
-  return render_template('home.html', home_menu=home, coin_data=data)
+  # get user's IP
+  ip = request.remote_addr
+  # hostname = socket.gethostname()
+  # ip_address = socket.gethostbyname(hostname)
+  # ip_address = request.environ.get('REMOTE_ADDR')
+
+  return render_template('home.html', home_menu=home, coin_data=data, ip=ip)
